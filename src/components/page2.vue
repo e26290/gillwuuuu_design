158a6746d542fa9064df5c90b62c7392929cd6cf
gill <e26290@gmail.com>
2023-12-09 10:28:09 +0800

    feat:測試成功

---
+++ src/components/page2.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Page 244444444</h1>
+        <p>This is Page 2.</p>
+        <router-link to="/page1">Go back to Page 1</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
